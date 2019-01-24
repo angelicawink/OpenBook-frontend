4a3e54f46809a9c9d4689fa0e8ca1475dd51b571
Angelica Winkler <carowink3@gmail.com>
2019-01-24 17:43:54 -0500

    refactor of EnvelopeGif.js

--- src/Components/EnvelopeGif.js
+++ src/Components/EnvelopeGif.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class EnvelopeGif extends Component {
-  render(){
-    return(
-      <>
-        <img
+const EnvelopeGif = props => {
+  return (
+    <>
+      <img
         src="https://d3dvldql7ksohz.cloudfront.net/000_clients/646957/page/6469574kJDviXu.gif"
         alt="envelope"
         id="envelope-gif"
-        onClick={this.props.handleClick}
-        />
-      </>
-    )
-  }
-}
+        onClick={props.handleClick}
+      />
+    </>
+  );
+};
 
-export default EnvelopeGif
+export default EnvelopeGif;
