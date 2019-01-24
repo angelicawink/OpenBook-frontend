6785331e48993f711bc06ecad60502210e731b38
Angelica Winkler <carowink3@gmail.com>
2019-01-24 17:42:38 -0500

    refactor of EnvelopeClosed.js

--- src/Components/EnvelopeClosed.js
+++ src/Components/EnvelopeClosed.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class EnvelopeClosed extends Component{
-  render(){
-    return(
+const EnvelopeClosed = props => {
+  return (
+    <div className="envelope-stamp">
+      <div id="envelope" onClick={props.handleClick}>
+        <img
+          id="postage"
+          src="https://www.postagehq.com/wp-content/uploads/2018/08/postage-stamps-icon-150x150.png"
+          alt="stamp"
+        />
 
-      <div className="envelope-stamp">
-        
-        <div
-          id="envelope"
-          onClick={this.props.handleClick}>
-
-          <img
-            id="postage"
-            src="https://www.postagehq.com/wp-content/uploads/2018/08/postage-stamps-icon-150x150.png" alt="stamp"/>
-
-          <div id="address">
-            7200 Oblivion Boulevard
-          </div>
-
-        </div>
+        <div id="address">7200 Oblivion Boulevard</div>
       </div>
+    </div>
+  );
+};
 
-    )
-  }
-}
-
-export default EnvelopeClosed
+export default EnvelopeClosed;
--- src/actions/openBookActions.js
+++
@@ -1,7 +0,0 @@
-function fetchedUser(data) {
-  return {type: "FETCHED_USER", payload: data}
-}
-
-export { fetchedUser }
-
-  
