6a95bf313a94f4f08c716d53273aac64f720fa13
Angelica Winkler <carowink3@gmail.com>
2019-01-28 09:27:00 -0500

    refactored LetterDone.js

--- src/Components/LetterDone.js
+++ src/Components/LetterDone.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class LetterDone extends Component{
-  constructor(props){
-    super(props)
-    this.state={
-      render:false
-    }
+class LetterDone extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      render: false
+    };
   }
 
-componentDidMount(){
-  setTimeout(this.setRendertoTrue, 1200)
-}
+  componentDidMount() {
+    setTimeout(this.setRendertoTrue, 1200);
+  }
 
-setRendertoTrue = () => {
-  this.setState({
-    render: true
-  })
-}
+  setRendertoTrue = () => {
+    this.setState({ render: true });
+  };
 
-  render(){
-    return(
+  render() {
+    return (
       <div id="done">
         Ah. That feels better.
-
         <div>
-          {this.state.render ?
-            <button id="write-another" onClick={this.props.handleClick}>Rage again.</button>
-            :null}
+          {this.state.render ? (
+            <button id="write-another" onClick={this.props.handleClick}>
+              Rage again.
+            </button>
+          ) : null}
         </div>
       </div>
-
-    )
+    );
   }
 }
 
-export default LetterDone
+export default LetterDone;
