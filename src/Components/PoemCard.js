3fcd58f170572518d4568bfe4ab8373940a993e9
Angelica Winkler <carowink3@gmail.com>
2019-01-28 09:45:45 -0500

    refactored Poetry.js

--- src/Components/PoemCard.js
+++ src/Components/PoemCard.js
@@ -1,34 +1,30 @@
-import React, { Component} from 'react';
-import { Card } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 
+class PoemCard extends Component {
+  parsePoemContent = () => {
+    var poemLines = this.props.poem.content.split(/[,.]/);
+    return poemLines;
+  };
 
-class PoemCard extends Component{
-
-parsePoemContent = () => {
-  var poemLines = this.props.poem.content.split((/[,.]/))
-  return poemLines
-}
-
-  render(){
-    return(
+  render() {
+    return (
       <div className="col-xs-4">
-        <br></br>
-      <Card style={{backgroundColor: this.props.color}} color="yellow">
-        <Card.Content>
-          <Card.Header>{this.props.poem.title}</Card.Header>
-          {this.parsePoemContent().map((line, index) =>
-            <Card.Meta
-              className="line"
-              key={index}>
-              {line}
-            </Card.Meta>)}
-        </Card.Content>
-      </Card>
-        <br></br>
-
-    </div>
-    )
+        <br />
+        <Card style={{ backgroundColor: this.props.color }} color="yellow">
+          <Card.Content>
+            <Card.Header>{this.props.poem.title}</Card.Header>
+            {this.parsePoemContent().map((line, index) => (
+              <Card.Meta className="line" key={index}>
+                {line}
+              </Card.Meta>
+            ))}
+          </Card.Content>
+        </Card>
+        <br />
+      </div>
+    );
   }
 }
 
-export default PoemCard
+export default PoemCard;
--- src/Components/Poetry.js
+++ src/Components/Poetry.js
@@ -1,53 +1,56 @@
-import React, { Component } from 'react';
-import Poem from './Poem';
-import RhymeFinder from './RhymeFinder';
+import React, { Component } from "react";
+import Poem from "./Poem";
+import RhymeFinder from "./RhymeFinder";
 
-class Poetry extends Component{
-  state={
-    showPoemForm: true,
-    wordToAdd: null
+class Poetry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPoemForm: true,
+      wordToAdd: null
+    };
   }
 
-  addWord = (word) => {
-    this.setState({
-      wordToAdd: word
-    })
-  }
+  addWord = word => {
+    this.setState({ wordToAdd: word });
+  };
 
   togglePoetryDisplay = () => {
     this.setState({
       showPoemForm: !this.state.showPoemForm
-    })
-  }
+    });
+  };
 
-  render(){
-    return(
+  render() {
+    return (
       <>
-        {this.state.showPoemForm ?
-
+        {this.state.showPoemForm ? (
           <div className="container poetry">
             <Poem
               addPoem={this.props.addPoem}
               user={this.props.user}
               wordToAdd={this.state.wordToAdd}
-              togglePoetryDisplay={this.togglePoetryDisplay}/>
-            <RhymeFinder
-              addWord={this.addWord}/>
+              togglePoetryDisplay={this.togglePoetryDisplay}
+            />
+            <RhymeFinder addWord={this.addWord} />
           </div>
-
-          :
+        ) : (
           <div className="container poem-done">
-            <h2>
-              Wow. Amazing.
-            </h2>
-            <button className="new-entry" onClick={this.togglePoetryDisplay}>Write another</button>
-            <button className="new-entry" onClick={() => this.props.history.push('/wallow')}>Go to my poems</button>
+            <h2>Wow. Amazing.</h2>
+            <button className="new-entry" onClick={this.togglePoetryDisplay}>
+              Write another
+            </button>
+            <button
+              className="new-entry"
+              onClick={() => this.props.history.push("/wallow")}
+            >
+              Go to my poems
+            </button>
           </div>
-        }
-
+        )}
       </>
-    )
+    );
   }
 }
 
-export default Poetry
+export default Poetry;
